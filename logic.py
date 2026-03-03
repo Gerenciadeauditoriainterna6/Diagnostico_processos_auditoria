b5fa14f5b7dbeee4556f11c3882c07166d19e2f1
Gerenciadeauditoriainterna6 <gerenciadeauditoriainterna6@fusve.org.br>
2026-03-03 15:48:02 -0300

    correção de bugs

--- logic.py
+++ logic.py
@@ -138,11 +138,6 @@ def buscar_dados_do_processo(codigo_processo):
     with engine.connect() as conn:
         return pd.read_sql(query, conn, params={"codigo": codigo_processo})
 
-def buscar_processos_pendentes():
-    # Retorna o código, que será mais amigável
-    query = "SELECT codigo_processo, area, nome_processo FROM processos" 
-    return pd.read_sql(query, engine)
-
 def gerar_pdf_em_memoria(id_proc):
     df_processo = buscar_dados_do_processo(id_proc)
     if df_processo.empty: return None
