308a2d1f77435341b59efcfa893092c06d93d8ec
Gerenciadeauditoriainterna6 <gerenciadeauditoriainterna6@fusve.org.br>
2026-03-03 10:26:09 -0300

    remove get_engine function and initialize engine directly

--- database.py
+++ database.py
@@ -2,6 +2,5 @@ import streamlit as st
 from sqlalchemy import create_engine
 
 # A conexão fica centralizada aqui
-def get_engine():
-    db_url = st.secrets["connections"]["url"]
-    return create_engine(db_url)
+db_url = st.secrets["connections"]["url"]
+engine = create_engine(db_url)
