f6e3644865818495f08d87c77bbadadd1660e81c
Gerenciadeauditoriainterna6 <gerenciadeauditoriainterna6@fusve.org.br>
2026-03-12 10:09:59 -0300

    correcao de bug

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ listar_riscos_etapa, buscar_todos_processos, salvar_controle_no_banco, validar_l
 )
 import base64
 
+# --- 1. CONFIGURAÇÃO INICIAL ---
+st.set_page_config(page_title="Diagnóstico FUSVE", layout="centered")
+
 def get_base64(bin_file):
     """Lê um arquivo de imagem e retorna sua versão codificada em Base64"""
     with open(bin_file, 'rb') as f:
@@ -23,15 +26,13 @@ def get_base64(bin_file):
 # estejam na mesma pasta do seu script Python.
 
 try:
-    bin_fundo = get_base64(r"\assets\imagem_fundo.png")  # Substitua pelo nome do seu arquivo de fundo
-    bin_logo = get_base64(r"\assets\logo_auditoria_recortada_circulo.png")    # Substitua pelo nome da sua logo
+    bin_fundo = get_base64(r"assets\imagem_fundo.png")  # Substitua pelo nome do seu arquivo de fundo
+    bin_logo = get_base64(r"assets\logo_auditoria_recortada_circulo.png")    # Substitua pelo nome da sua logo
 except FileNotFoundError:
     st.error("Erro: Arquivos de imagem não encontrados. Verifique os nomes 'fundo.jpg' e 'logo.png'.")
     bin_fundo = ""
     bin_logo = ""
 
-# --- 1. CONFIGURAÇÃO INICIAL ---
-st.set_page_config(page_title="Diagnóstico FUSVE", layout="centered")
 
 def login_screen(bin_fundo, bin_logo):
     # 1. CSS e HTML para Configuração de Layout e Estilo
@@ -129,9 +130,6 @@ def login_screen(bin_fundo, bin_logo):
             st.rerun()
         else:
             st.error("Usuário ou senha incorretos.")
-
-# 1. Primeiro de tudo, a configuração da página (DEVE ser a primeira linha do Streamlit)
-st.set_page_config(layout="wide", page_title="Sistema de Auditoria", page_icon="🔐")
 
 
 # 2. Chame a função para exibir a tela de login
