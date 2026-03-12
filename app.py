6999aaaa97b3802afb430bb0f603145ec89078c3
Gerenciadeauditoriainterna6 <gerenciadeauditoriainterna6@fusve.org.br>
2026-03-12 14:50:41 -0300

    teste

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def login_screen():
                 <div style="text-align: center; width: 100%; line-height: 1.2;">
                     <span style="color: white; font-family: sans-serif; font-size: 12px; display: block;">SISTEMA</span>
                     <span style="color: white; font-family: sans-serif; font-size: 14px; font-weight: bold; display: block;">GERÊNCIA DE AUDITORIA INTERNA</span>
-                    <span style="color: #822a2d; font-family: sans-serif; font-size: 10px; font-weight: bold; display: block; margin-top: 20px; margin-bottom: 0px;">Acesso Restrito!</span>
+                    <span style="color: #822a2d; font-family: sans-serif; font-size: 10px; font-weight: bold; display: block; margin-top: 10px; margin-bottom: 0px;">Acesso Restrito!</span>
                 </div>
             ''', unsafe_allow_html=True)
 
