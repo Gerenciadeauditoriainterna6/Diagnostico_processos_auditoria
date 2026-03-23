1ade65cf25fb9a3112d9d67e20bca097c8baac84
Gerenciadeauditoriainterna6 <gerenciadeauditoriainterna6@fusve.org.br>
2026-03-23 11:41:02 -0300

    correcao de bug no botao de salvar informacoes basicas

--- app.py
+++ app.py
@@ -1777,16 +1777,16 @@ def main():
                 funcionarios_lista = listar_funcionarios_por_area(id_area_atual)
 
             if not funcionarios_lista:
-                st.warning("⚠️ Nenhum funcionário cadastrado para esta área. Cadastre funcionários em '🏢 Cadastro de Áreas'.")
-                if 'novo_executores_selecionados' not in st.session_state:
-                    st.session_state['novo_executores_selecionados'] = []
+                st.warning("⚠️ Nenhum funcionário cadastrado para esta área. Cadastre funcionários em '🏢 Cadastro de Áreas e Funcionários'.")
+                if 'nexecutores_selecionados' not in st.session_state:
+                    st.session_state['executores_selecionados'] = []
             else:
                 funcionarios_ids = [f[0] for f in funcionarios_lista]
                 funcionarios_dict = {f[0]: f[1] for f in funcionarios_lista}
                 
                 defaults_validos = []
-                if 'edit_executores_selecionados' in st.session_state:
-                    for exec_id in st.session_state['edit_executores_selecionados']:
+                if 'executores_selecionados' in st.session_state:
+                    for exec_id in st.session_state['executores_selecionados']:
                         if exec_id in funcionarios_dict:
                             defaults_validos.append(exec_id)
 
@@ -1795,12 +1795,12 @@ def main():
                     options=funcionarios_ids,
                     format_func=lambda x: funcionarios_dict[x],
                     default=defaults_validos,
-                    key="edit_multiselect_executores",
+                    key="multiselect_executores",
                     help="Você pode selecionar um ou mais funcionários",
                     placeholder="Selecione os funcionários que executam este processo:"
                 )
                 
-                st.session_state['edit_executores_selecionados'] = selecionados
+                st.session_state['executores_selecionados'] = selecionados
                 
                 if selecionados:
                     nomes_selecionados = [funcionarios_dict[id] for id in selecionados]
